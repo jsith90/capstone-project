445bbbda08d9b32482be10e89741f96ada574fc6
Josh Sinclair <j.sinclairthomson@gmail.com>
2024-03-20 15:20:30 +0000

    adjust code to pass validaion tests

--- members/views.py
+++ members/views.py
@@ -7,71 +7,76 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import update_session_auth_hash
 
+
 def login_user(request):
-	if request.method == "POST":
-		username = request.POST["username"]
-		password = request.POST["password"]
-		user = authenticate(request, username=username, password=password)
-		if user is not None:
-			login(request, user)
-			messages.success(request, ("Welcome back!"))
-			return redirect('index')
-		else:
-			messages.success(request, ("There was an error logging in, try again..."))
-			return redirect('login_user')
-	else:
-		return render(request, 'authenticate/login.html', {})
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("Welcome back!"))
+            return redirect('index')
+        else:
+            messages.success(request, ("There was an error logging in, try again..."))  # noqa
+            return redirect('login_user')
+    else:
+        return render(request, 'authenticate/login.html', {})
+
 
 def logout_user(request):
-	logout(request)
-	messages.success(request, ("You logged out!"))
-	return redirect('index')
+    logout(request)
+    messages.success(request, ("You logged out!"))
+    return redirect('index')
+
 
 def register_user(request):
-	if request.method == "POST":
-		form = RegisterUserForm(request.POST)
-		if form.is_valid():
-			form.save()
-			username = form.cleaned_data['username']
-			password = form.cleaned_data['password1']
-			user = authenticate(username=username, password=password)
-			login(request, user)
-			messages.success(request, ("Registration successful!"))
-			return redirect('index')
-	else:
-		form = RegisterUserForm()
-	return render(request, 'authenticate/register_user.html', {
-		'form':form,
-		})
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']  # noqa
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Registration successful!"))
+            return redirect('index')
+    else:
+        form = RegisterUserForm()
+    return render(request, 'authenticate/register_user.html', {
+        'form': form,
+    })
+
 
 def details_update(request, id):
-	user = User.objects.get(pk=id)
-	if request.user.is_authenticated:	
-		if request.user == user:
-			if request.method == 'POST':
-				form = UserUpdateForm(request.POST, instance=user)
-				if form.is_valid():
-					form.save()
-					messages.success(request, "User details updated successfully.")
-					return redirect('user_panel')
-			else:
-				form = UserUpdateForm(instance=user)
-			return render(request, 'authenticate/details_update.html', {'form': form})
-		else:
-			messages.success(request, 'You are not authorised to do that.')
-			return render(request, 'booking/index.html')
-	else:
-		messages.success(request, 'Please log-in to make changes to your profile.')
-		return redirect('login_user')
+    user = User.objects.get(pk=id)
+    if request.user.is_authenticated:
+        if request.user == user:
+            if request.method == 'POST':
+                form = UserUpdateForm(request.POST, instance=user)
+                if form.is_valid():
+                    form.save()
+                    messages.success(request, "User details updated successfully.")  # noqa
+                    return redirect('user_panel')
+            else:
+                form = UserUpdateForm(instance=user)
+            return render(request, 'authenticate/details_update.html', {'form': form})  # noqa
+        else:
+            messages.success(request, 'You are not authorised to do that.')
+            return render(request, 'booking/index.html')
+    else:
+        messages.success(request, 'Please log-in to make changes to your profile.')  # noqa
+        return redirect('login_user')
+
 
 def password_update(request):
-	if request.method == 'POST':
-		form = PasswordUpdateForm(request.user, request.POST)
-		if form.is_valid():
-			user = form.save()
-			update_session_auth_hash(request, user)
-			messages.success(request, "Password successfully updated!")
-			return redirect('user_panel')
-	else:
-		form = PasswordUpdateForm(request.user)
-	return render(request, 'authenticate/password_update.html', {'form': form})
+    if request.method == 'POST':
+        form = PasswordUpdateForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, "Password successfully updated!")
+            return redirect('user_panel')
+    else:
+        form = PasswordUpdateForm(request.user)
+    return render(request, 'authenticate/password_update.html', {'form': form})
